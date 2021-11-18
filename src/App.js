191b6938dea019ede8cb8790f6da565c3440a7db
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-11-18 07:14:06 +0600

    Project set up done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Home from './Pages/CommonPages/Home/Home/Home';
+import Product from './Pages/CommonPages/Product/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/products">
+            <Product></Product>
+          </Route>
+          {/* <Route exact path="/transports">
+              <Transports></Transports>
+            </Route>
+            <Route exact path="/placeOrder/:packageId">
+              <PlaceOrder></PlaceOrder>
+            </Route>
+            <Route exact path="/myOrder">
+              <MyOrder></MyOrder>
+            </Route>
+            <Route exact path="/manageOrder">
+              <ManageOrder></ManageOrder>
+            </Route>
+            <Route exact path="/addService">
+              <AddService></AddService>
+            </Route>
+            <Route exact path="/login">
+              <Login></Login>
+            </Route> */}
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+
+        </Switch>
+
+        <Footer></Footer>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/Pages/PrivatePagesUser/SendReview/SendReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SendReview = () => {
+    return (
+        <div>
+            <h1>Send Review</h1>
+        </div>
+    );
+};
+
+export default SendReview;
