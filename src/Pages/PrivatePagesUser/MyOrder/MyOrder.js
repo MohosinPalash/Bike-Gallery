5fa6d55f3d651b6f54302e69488dad38c8c53d1b
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-11-19 19:15:45 +0600

    My Order for User done

--- src/Pages/PrivatePagesUser/MyOrder/MyOrder.js
+++ src/Pages/PrivatePagesUser/MyOrder/MyOrder.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import useAuth from '../../../contexts/AuthProvider/useAuth';
 
 const MyOrder = () => {
+    const { user, logout } = useAuth();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders/${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, [orders])
+
+    //Delete order
+    const handleDeleteOrder = id => {
+        const proceed = window.confirm('Are you sure you want to cancel the order?');
+        if (proceed) {
+            const url = `http://localhost:5000/orders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Order Cancelled Successfully!');
+                        const remainingOrder = orders.filter(order => order._id != id)
+                        setOrders(remainingOrder);
+                    }
+                })
+        }
+    }
     return (
-        <div>
-            <h1>My Order</h1>
-        </div>
+        orders.length > 0 ?
+            <div className="container">
+                <h2 style={{ fontWeight: '600' }}>My Orders</h2>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Email Address</th>
+                            <th>Contact No</th>
+                            <th>Customer Name</th>
+                            <th>Product Name</th>
+                            <th>Fare</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    {
+                        orders.map((order, index) =>
+                            <tbody>
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{order.email}</td>
+                                    <td>{order.contact}</td>
+                                    <td>{order.name}</td>
+                                    <td>{order.title}</td>
+                                    <td>{order.Price}</td>
+                                    <td>{order.status}</td>
+                                    {
+                                        order.status === 'Pending' ?
+                                            <td><button className="btn btn-danger" onClick={() => handleDeleteOrder(order._id)}>Cancel Order</button></td>
+                                            :
+                                            <td >Not Allowed</td>
+                                    }
+
+                                </tr>
+                            </tbody>
+                        )
+
+                    }
+
+                </Table>
+            </div >
+            :
+            <p>No orders Found!</p>
     );
 };
 
