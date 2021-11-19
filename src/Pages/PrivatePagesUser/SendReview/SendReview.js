52ba1d77f384c5cdf853b7f1abf66c3f508443ca
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-11-19 21:16:14 +0600

    Footer Added

--- src/Pages/PrivatePagesUser/SendReview/SendReview.js
+++ src/Pages/PrivatePagesUser/SendReview/SendReview.js
@@ -1,10 +1,50 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../contexts/AuthProvider/useAuth';
+import cover1 from '../../../images/cover1.png';
 
 const SendReview = () => {
+    const { user, logout } = useAuth();
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        data.email = user.email;
+        data.name = user.displayName;
+        console.log('data', data);
+        axios.post('http://localhost:5000/review', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    reset();
+                    document.getElementById('confirm-msg').style.display = "block";
+                }
+            })
+    };
     return (
-        <div>
-            <h1>Send Review</h1>
-        </div>
+        <>
+
+            <div className="place-order-grid">
+                <div>
+
+                    <div className="place-order">
+
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <textarea type="text"{...register("review", { required: true, maxLength: 50 })} placeholder="Write your review here" />
+                            <input type="number" {...register("rating", { required: true, maxLength: 100 })} placeholder="Rate us (1 to 5)" />
+                            <input type="submit" className="submit-button" value="SEND REVIEW" />
+                            <p id="confirm-msg" style={{ color: 'green', display: 'none' }}>Your review is sent successfully!
+                                <Link to={'/home'}><strong>Go to home</strong></Link>
+                            </p>
+                        </form>
+
+                    </div>
+                </div>
+                <div className="login-info">
+                    <img src={cover1} alt="" />
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -38,15 +38,7 @@ const Header = () => {
 
                                 <Navbar.Text style={{ color: "white", cursor: "pointer" }} onClick={logout}>Logout</Navbar.Text>
                                 <Navbar.Text>
-                                    <Nav.Link as={Link} to="/home" style={{ marginTop: "0" }}>Welcome USER
-                                        {/* {user?.displayName.split(" ")[0]}<img src={user?.photoURL} style={{
-                                        height: "40px",
-                                        width: "40px",
-                                        borderRadius: "50%",
-                                        border: "2px solid white",
-                                        marginLeft: "10px",
-                                        margin: "-15px 10px"
-                                    }} alt="" /> */}
+                                    <Nav.Link as={Link} to="/home" style={{ marginTop: "0" }}>Welcome {user?.displayName.split(" ")[0]}
                                     </Nav.Link>
                                 </Navbar.Text>
                             </>
